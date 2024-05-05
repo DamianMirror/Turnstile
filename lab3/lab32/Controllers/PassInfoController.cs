52ddc9ae1b79b251e1be993c4d24f4a99d5c509b
Damian <dematchuma2017@gmail.com>
2024-05-05 12:56:17 +0300

    asd
    ad

---
+++ lab3/lab32/Controllers/PassInfoController.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+
+namespace lab32;
+
+[ApiController]
+[Route("[controller]")]
+public class PassInfoController : ControllerBase
+{
+    [HttpPost("create-pass")]
+    public IActionResult CreatePass(string name, Status status, PassTypes passType, int securityLevel = 1, int passesAmountLeft = 0)
+    {
+        PassInfo pass = new PassInfo(name, status, passType, securityLevel, passesAmountLeft);
+        UsersBase.Users.Add(pass);
+        return Ok();
+    }
+}
